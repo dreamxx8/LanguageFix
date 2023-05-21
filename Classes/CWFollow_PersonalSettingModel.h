c92a838526d382d9675b7758b09823cdafe1085d
dreamxx8 <631111567@qq.com>
2023-05-21 22:20:09 +0800

    Update CWFollow_PersonalSettingModel.h

--- Classes/CWFollow_PersonalSettingModel.h
+++ Classes/CWFollow_PersonalSettingModel.h
@@ -6,11 +6,9 @@
 //  Copyright © 2022 CoinW. All rights reserved.
 //
 
-#import "CWBaseModel.h"
 
 
-
-@interface CWFollow_PersonalSettingModel : CWBaseModel
+@interface CWFollow_PersonalSettingModel : NSObject
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subTitle;
 @property (nonatomic, copy) NSString *imageURL;
